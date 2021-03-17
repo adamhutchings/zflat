0cb43de61c115bc6bfca6e3cc89ad644e48c2e6e
adamhutchings <adam.abahot@gmail.com>
2021-03-16 23:47:58 -0700

    Add tree fragments

---
+++ zfc/front/trfrags.hpp
@@ -0,0 +1,37 @@
+/**
+ * Tree fragments - types for tokens and tree fragments.
+ */
+
+#pragma once
+
+enum TreeComp {
+
+    // Token types
+    IDENTIFIER,
+    TYPENAME,
+    LITERAL,
+    OPERATOR,
+    SEMICOLON,
+    OBRACE,
+    CBRACE,
+    OPAREN,
+    CPAREN,
+    OBRACKET,
+    CBRACKET,
+    COLON,
+    COMMA,
+    DOT,
+    FLOW, // break, continue return
+
+    // Tree fragments
+    EXPRESSION,
+    BARE_VAR_DECL, // x: int
+    FULL_VAR_DECL, // x: int = 3
+    FLOW_STATEMENT, // break, return, continue
+    STATEMENT,
+    BLOCK_STATEMENT,
+    FUNCTION_DECL,
+    USE_STATEMENT,
+    PROGRAM,
+
+};
