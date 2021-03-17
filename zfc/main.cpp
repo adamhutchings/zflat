80776b9970d41e80ab33bd3660581a15cbe079df
adamhutchings <adam.abahot@gmail.com>
2021-03-16 23:34:03 -0700

    Test the lexer (broken)

--- zfc/main.cpp
+++ zfc/main.cpp
@@ -2,6 +2,24 @@
  * main - main file of the ZFC compiler.
  */
 
+#include <iostream>
+
+#include <front/lex.hpp>
+
 int main(int argc, char** argv) {
-    
+    if (argc == 1) {
+        std::cerr << "Expected file" << "\n";
+        exit(-1);
+    }
+    std::ifstream file;
+    file.open(argv[1]);
+    if (!file) {
+        std::cerr << "Cannot open " << argv[1]  << "\n";
+        exit(-1);
+    }
+    Token tok;
+    while (lex(file, tok).type != TokenType::TEOF) {
+        printf("%-10s   %-10d\n", tok.str.c_str(), tok.type);
+    }
+    printf("Lexed %d tokens\n", tokens);
 }
