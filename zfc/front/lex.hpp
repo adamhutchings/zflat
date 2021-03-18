b30caed6f799589af36abf6c9d189108b6e9cc1b
adamhutchings <adam.abahot@gmail.com>
2021-03-18 08:51:10 -0700

    Add lexer interface

--- zfc/front/lex.hpp
+++ zfc/front/lex.hpp
@@ -4,4 +4,24 @@
 
 #pragma once
 
+#include <fstream>
+#include <string>
+#include <vector>
 
+#include <front/trfrags.hpp>
+
+struct Token {
+
+    TreeComp type;
+    std::string str;
+    int line;
+    int linepos;
+
+    inline const char* raw_content();
+    Token(TreeComp t, std::string s);
+
+};
+
+namespace lex {
+    Token lex(std::ifstream& file);
+}
