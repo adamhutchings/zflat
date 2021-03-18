3ab1831600673a6e29285bfed734aa3242f68aa1
adamhutchings <adam.abahot@gmail.com>
2021-03-18 16:51:20 -0700

    Lexer test

--- zfc/main.cpp
+++ zfc/main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <iostream>
 
+#include <front/lex.hpp>
+
 int main(int argc, char** argv) {
     if (argc == 1) {
         std::cerr << "Expected file" << "\n";
@@ -12,4 +14,12 @@ int main(int argc, char** argv) {
     }
     std::ifstream file;
     file.open(argv[1]);
+    if (!file) {
+        std::cerr << "Unable to open " << argv[1] << "\n";
+        exit(-1);
+    }
+    Token tok;
+    while ( (tok = lex::lex(file)).type != TreeComp::TEOF ) {
+        std::cout << "token: " << tok.str << "\n";
+    }
 }
