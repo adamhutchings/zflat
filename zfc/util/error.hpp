7f500b1f63671443573cb572003b45f079bbdc21
adamhutchings <adam.abahot@gmail.com>
2021-03-20 22:44:44 -0700

    Add newline

--- zfc/util/error.hpp
+++ zfc/util/error.hpp
@@ -9,6 +9,6 @@
 
 #define ZF_ERROR(msg, ...) do { \
     fprintf(stderr, "zfc: "); \
-    fprintf(stderr, msg ZF_ARGS(__VA_ARGS__)); \
+    fprintf(stderr, msg "\n" ZF_ARGS(__VA_ARGS__)); \
     exit(-1); \
 } while (0)
