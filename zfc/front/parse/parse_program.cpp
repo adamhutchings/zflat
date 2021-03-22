9eb5de47fef7ebf9c6417a1827ffec916f46816a
adamhutchings <adam.abahot@gmail.com>
2021-03-21 21:24:54 -0700

    Make code even more cursed to check for fndef

--- zfc/front/parse/parse_program.cpp
+++ zfc/front/parse/parse_program.cpp
@@ -16,16 +16,23 @@ void ProgramNode::read(std::ifstream& file) {
         lex::unlex(endcheck);
 
         // Quick and dirty function definition check - check if second token is open paren
+        // and fourth is colon
         auto first = lex::lex(file);
         auto opn = lex::lex(file);
+        auto un = lex::lex(file);
+        auto cl = lex::lex(file);
 
-        if (opn.type == TreeComp::OPAREN) {
+        if (opn.type == TreeComp::OPAREN && cl.type == TreeComp::COLON) {
+            lex::unlex(cl);
+            lex::unlex(un);
             lex::unlex(opn);
             lex::unlex(first);
             FunctionNode* node = new FunctionNode();
             node->read(file);
             this->components.push_back(node);
         } else {
+            lex::unlex(cl);
+            lex::unlex(un);
             lex::unlex(opn);
             lex::unlex(first);
             StatementNode* node = new StatementNode();
