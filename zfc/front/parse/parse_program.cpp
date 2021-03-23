c243ea42290d290475296cebcbce2c954dc919ec
adamhutchings <adam.abahot@gmail.com>
2021-03-22 21:58:47 -0700

    Remove a bit of hackiness

--- zfc/front/parse/parse_program.cpp
+++ zfc/front/parse/parse_program.cpp
@@ -4,6 +4,29 @@
 #include <front/lex.hpp>
 #include <util/error.hpp>
 
+bool check_fn(std::ifstream& file) {
+
+    // check if next is a function definition
+
+    // Quick and dirty function definition check - check if second token is open paren
+    // and fourth is colon
+    auto first = lex::lex(file);
+    auto opn = lex::lex(file);
+    auto un = lex::lex(file);
+    auto cl = lex::lex(file);
+
+    return (first.type == TreeComp::IDENTIFIER
+    && opn == TreeComp::OPAREN
+    && un == TreeComp::IDENTIFIER
+    && cl == TreeComp::COLON);
+
+    lex::unlex(cl);
+    lex::unlex(un);
+    lex::unlex(opn);
+    lex::unlex(first);
+
+}
+
 void ProgramNode::read(std::ifstream& file) {
 
     while (1) {
@@ -15,26 +38,11 @@ void ProgramNode::read(std::ifstream& file) {
 
         lex::unlex(endcheck);
 
-        // Quick and dirty function definition check - check if second token is open paren
-        // and fourth is colon
-        auto first = lex::lex(file);
-        auto opn = lex::lex(file);
-        auto un = lex::lex(file);
-        auto cl = lex::lex(file);
-
-        if (opn.type == TreeComp::OPAREN && cl.type == TreeComp::COLON) {
-            lex::unlex(cl);
-            lex::unlex(un);
-            lex::unlex(opn);
-            lex::unlex(first);
+        if (check_fn(file)) {
             FunctionNode* node = new FunctionNode();
             node->read(file);
             this->components.push_back(node);
         } else {
-            lex::unlex(cl);
-            lex::unlex(un);
-            lex::unlex(opn);
-            lex::unlex(first);
             StatementNode* node = new StatementNode();
             node->read(file);
             this->components.push_back(node);
