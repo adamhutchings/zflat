c427316da63826f3c113ac757c81456c50d94410
adamhutchings <adam.abahot@gmail.com>
2021-03-23 23:24:39 -0700

    Add block statement generation

---
+++ zfc/back/gen/gen_blockstat.cpp
@@ -0,0 +1,15 @@
+#include <ast/ast.hpp>
+#include <gen_main.hpp>
+
+void BlockStatementNode::write(std::ofstream& file) {
+
+    gen::writeln(file, " {");
+    gen::indent();
+    
+    for (auto stmt : this->statements)
+        stmt->write(file);
+
+    gen::unindent();
+    gen::writeln(file, "}");
+
+}
