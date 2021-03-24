8d5115c9233179ff25b7b0d306b3d730e579d943
adamhutchings <adam.abahot@gmail.com>
2021-03-23 23:26:12 -0700

    Add function call gen

---
+++ zfc/back/gen/gen_funccall.cpp
@@ -0,0 +1,16 @@
+#include <ast/ast.hpp>
+#include <gen_main.hpp>
+
+void FuncCallNode::write(std::ofstream& file) {
+
+    gen::write(file, this->name);
+    gen::write(file, "(");
+
+    for (auto arg : this->args) {
+        arg->write(file);
+        gen::write(file, ",");
+    }
+
+    gen::write(file, ")");
+
+}
