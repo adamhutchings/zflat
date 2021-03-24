8cd23d28892372955c05a2881de2f976f3403c40
adamhutchings <adam.abahot@gmail.com>
2021-03-23 23:56:41 -0700

    Fix file opening

--- zfc/compile.cpp
+++ zfc/compile.cpp
@@ -3,11 +3,11 @@
 void compile(std::string in, std::string out) {
     std::ifstream ifile;
     ifile.open(in);
-    if (ifile.bad())
+    if (!ifile)
         ZF_ERROR("Could not open %s for reading", in.c_str());
     std::ofstream ofile;
     ofile.open(out);
-    if (ofile.bad())
+    if (!ofile)
         ZF_ERROR("Could not open %s for writing", out.c_str());
     ProgramNode pnode;
     pnode.read(ifile);
