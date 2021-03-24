32f04e6ed8b159626c87f6feaefd0f3c4a843778
adamhutchings <adam.abahot@gmail.com>
2021-03-23 23:51:36 -0700

    Fix some namespaces

--- zfc/back/gen/gen_main.cpp
+++ zfc/back/gen/gen_main.cpp
@@ -6,9 +6,7 @@ int indentation_level = 0;
 
 }
 
-namespace gen {
-
-void write(std::ostream& file, std::string str) {
+void gen::write(std::ostream& file, std::string str) {
 
     // tabs
     for (int i = 0; i < indentation_level; ++i)
@@ -18,19 +16,17 @@ void write(std::ostream& file, std::string str) {
 
 }
 
-void writeln(std::ofstream& file, std::string str) {
+void gen::writeln(std::ofstream& file, std::string str) {
     write(file, str);
     file << "\t";
 }
 
-void indent() {
+void gen::indent() {
     ++indentation_level;
 }
 
-void unindent() {
+void gen::unindent() {
     --indentation_level;
     if (indentation_level < 0)
         ZF_BACK_ERR("indentation level went below zero in generated code");
-}
-
 }
