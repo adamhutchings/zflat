522bcb06b78b57404f1760415af030532c3a3adf
adamhutchings <adam.abahot@gmail.com>
2021-03-24 14:44:33 -0700

    Make formatting slightly better

--- zfc/back/gen/gen_blockstat.cpp
+++ zfc/back/gen/gen_blockstat.cpp
@@ -3,13 +3,14 @@
 
 void BlockStatementNode::write(std::ofstream& file) {
 
-    gen::writeln(file, " {");
+    gen::write(file," {");
     gen::indent();
+    gen::writeln(file, "");
     
     for (auto stmt : this->statements)
         stmt->write(file);
 
     gen::unindent();
-    gen::writeln(file, "}");
+    gen::writeln(file, "}\n");
 
 }
