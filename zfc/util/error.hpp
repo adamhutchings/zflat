5e84d1ec0f2514963a2f0680126fd203c787eb3d
adamhutchings <adam.abahot@gmail.com>
2021-03-25 09:15:23 -0700

    Make error macro close files

--- zfc/util/error.hpp
+++ zfc/util/error.hpp
@@ -2,13 +2,19 @@
  * Error - error routines.
  */
 
+#pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <util/fs.hpp>
 
 #define ZF_ARGS(...) , ##__VA_ARGS__
 
 #define ZF_ERROR(msg, ...) do { \
     fprintf(stderr, "zfc: "); \
     fprintf(stderr, msg "\n" ZF_ARGS(__VA_ARGS__)); \
+    fs::ifile.close();                              \
+    fs::ofile.close();                              \
     exit(-1); \
 } while (0)
