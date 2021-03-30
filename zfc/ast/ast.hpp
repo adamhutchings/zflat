2ce67b611c8732cf0c6f66e0d95803bd18993f87
adamhutchings <adam.abahot@gmail.com>
2021-03-29 23:17:02 -0700

    Add expr node type (coming!)

--- zfc/ast/ast.hpp
+++ zfc/ast/ast.hpp
@@ -95,6 +95,7 @@ struct ExprNode : public InnerStatementNode {
     void read(std::ifstream& file) override;
     void write(std::ofstream& file) override;
     virtual ~ExprNode();
+    std::string get_type();
     void validate() override;
 };
 
