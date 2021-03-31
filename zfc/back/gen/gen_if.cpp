32c70f56e2c5cee80ad5abe4649cd72f90f82da1
adamhutchings <adam.abahot@gmail.com>
2021-03-30 17:06:55 -0700

    Add if generation

---
+++ zfc/back/gen/gen_if.cpp
@@ -0,0 +1,15 @@
+#include <ast/ast.hpp>
+#include <back/gen/gen_main.hpp>
+
+void IfNode::write(std::ofstream& file) {
+
+    gen::write(file, "if (");
+    this->expr->write(file);
+    gen::write(file, ") ");
+    this->stmt->write(file);
+    if (this->else_block != nullptr) {
+        gen::write(file, "else");
+        this->else_block->write(file);
+    }
+
+}
