6e6ba46d585739dbc51ef7b715756d7238ae573f
adamhutchings <adam.abahot@gmail.com>
2021-03-30 18:13:17 -0700

    Add if statement detection

--- zfc/front/parse/parse_stat.cpp
+++ zfc/front/parse/parse_stat.cpp
@@ -39,6 +39,11 @@ void StatementNode::read(std::ifstream& file) {
             this->inner = new LoopNode();
             this->inner->read(file);
             goto out;
+        case TreeComp::IF:
+            lex::unlex(initial);
+            this->inner = new IfNode();
+            this->inner->read(file);
+            goto out;
         case TreeComp::SEMICOLON:
             lex::unlex(initial);
             this->inner = nullptr;
