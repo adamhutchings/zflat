4642862520d6b3497db567a06c1a0bebf49c93ac
adamhutchings <adam.abahot@gmail.com>
2021-03-31 18:45:35 -0700

    Apply function to nodes themselves

--- zfc/ast/ast.cpp
+++ zfc/ast/ast.cpp
@@ -9,18 +9,21 @@ ProgramNode::~ProgramNode() {
 }
 
 void ProgramNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     for (auto sub : this->components) {
         fn(sub);
     }
 }
 
 void FuncCallNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     for (auto arg : this->args) {
         fn(arg);
     }
 }
 
 void FunctionNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     for (auto decl : this->args) {
         fn(decl);
     }
@@ -28,36 +31,43 @@ void FunctionNode::apply(void(*fn)(ASTNode*)) {
 }
 
 void StatementNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     fn(this->inner);
 }
 
 void BlockStatementNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     for (auto stmt : this->statements) {
         fn(stmt);
     }
 }
 
 void IfNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     fn(this->expr);
     fn(this->stmt);
     if (this->else_block != nullptr) delete this->else_block;
 }
 
 void LoopNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     fn(this->expr);
     fn(this->pred);
     fn(this->stmt);
 }
 
 void ExprNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     if (this->left != nullptr) fn(this->left);
     if (this->right != nullptr) fn(this->left);
 }
 
 void ControlFlowNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     if (this->expression != nullptr) fn(this->expression);
 }
 
 void VarDeclNode::apply(void(*fn)(ASTNode*)) {
+    fn(this);
     if (this->expr != nullptr) fn(this->expr);
 }
