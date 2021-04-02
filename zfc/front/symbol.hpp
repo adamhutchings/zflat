c4c9d806cae093d46ad9787a2fc0bf7badee1ef8
adamhutchings <adam.abahot@gmail.com>
2021-04-01 17:07:22 -0700

    Add symbol code

---
+++ zfc/front/symbol.hpp
@@ -0,0 +1,38 @@
+/**
+ * symbol - symbols that AST nodes will have references to.
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace sym {
+
+struct Type {
+    std::string name;
+    inline Type(std::string n) : name(n) {}
+};
+
+struct Symbol {
+    std::string name;
+    int lineno;
+};
+
+struct Variable : public Symbol {
+    Type type;
+};
+
+struct Function : public Symbol {
+    std::vector<Type> args;
+    Type ret;
+};
+
+void enter_scope();
+void exit_scope();
+
+void add_symbol(Symbol* s);
+
+Symbol* resolve(std::string name);
+
+}
