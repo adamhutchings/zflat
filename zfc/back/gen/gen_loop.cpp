af9eec1e0aa482ce1e6b83327ef21475d0005abd
adamhutchings <adam.abahot@gmail.com>
2021-04-02 15:18:16 -0700

    Fix logic for while/for

--- zfc/back/gen/gen_loop.cpp
+++ zfc/back/gen/gen_loop.cpp
@@ -19,19 +19,19 @@ void LoopNode::write(std::ofstream& file) {
         if (this->pred == nullptr) {
             counterv = "lc_loop_";
             counterv += std::to_string(loopdepth);
-        } else if (type == BOOL) {
-            counterv = this->pred->var->name;
         } else {
-            ZF_ERROR("loop expr must be int or bool: line %d", this->line);
+            counterv = this->pred->var->name;
         }
         gen::write(file, begin + counterv + " = 0; " + counterv + " < " + this->expr->literal + "; ++" + counterv + ")");
         this->stmt->write(file);
-    } else {
+    }  else if (type == BOOL) {
         // Anything else is a boolean. For now.
         gen::write(file, "while (");
         this->expr->write(file);
         gen::write(file, ")");
         this->stmt->write(file);
+    } else {
+        ZF_ERROR("loop expr must be int or bool: line %d", this->line);
     }
 
     --loopdepth;
