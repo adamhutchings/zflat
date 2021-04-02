fe793d1005dd5c27fb1cb9af2cef7948ad92400d
adamhutchings <adam.abahot@gmail.com>
2021-04-02 13:41:48 -0700

    Fix -> to .

--- zfc/front/parse/parse_funccall.cpp
+++ zfc/front/parse/parse_funccall.cpp
@@ -20,7 +20,7 @@ void FuncCallNode::read(std::ifstream& file) {
     if (opn.type != TreeComp::OPAREN) {
         ZF_TOK_ERR(opn, "'('");
     }
-    this->line = opn->line;
+    this->line = opn.line;
 
     Token ctok = lex::lex(file);
 
