69a4e71e0567361d7eeb940e31f8b0cb24a849a2
adamhutchings <adam.abahot@gmail.com>
2021-04-04 09:24:41 -0700

    Check return type

--- zfc/front/parse/parse_cflow.cpp
+++ zfc/front/parse/parse_cflow.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 
 #include <ast/ast.hpp>
+#include <back/dtype.hpp>
 #include <front/lex.hpp>
 #include <util/error.hpp>
 
@@ -24,5 +25,12 @@ void ControlFlowNode::read(std::ifstream& file) {
         lex::unlex(next);
         this->expression = new ExprNode();
         this->expression->read(file);
+    }
+    // Check that return type is valid
+    if (
+        ( (this->expression == nullptr) ? Type::VOID : get_type(this->expression) )
+         != sym::current_function()->ret) {
+        ZF_ERROR("line %d: expected return type of %s, found expression of type %s"
+        , this->expression->line, typeToStr(get_type(this->expression)).c_str(), typeToStr(sym::current_function()->ret).c_str());
     }
 }
