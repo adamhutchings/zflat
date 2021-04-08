982b56933a765f129609ed330789329b518c7cfd
adamhutchings <adam.abahot@gmail.com>
2021-04-08 16:59:28 -0700

    Remove some unnecessary code

--- zfc/front/parse/parse_expr.cpp
+++ zfc/front/parse/parse_expr.cpp
@@ -48,15 +48,11 @@ void ExprNode::read(std::ifstream& file) {
                 left_expr->literal = start.str;
                 left_expr->ref = nullptr;
             } else {
-                if (start.str[0] >= '0' && start.str[0] >= '9') {
-                    left_expr->literal = start.str;
-                } else {
-                    left_expr->ref = sym::resolve(start.str);
-                    if (left_expr->ref == nullptr) {
-                        ZF_ERROR("could not resolve variable \"%s\" on line %d", start.raw_content(), start.line);
-                    }
-                    left_expr->literal = "";
+                left_expr->ref = sym::resolve(start.str);
+                if (left_expr->ref == nullptr) {
+                    ZF_ERROR("could not resolve variable \"%s\" on line %d", start.raw_content(), start.line);
                 }
+                left_expr->literal = "";
             }
             left_expr->left = left_expr->right = nullptr;
             left_expr->op = op::Operator::INVALID;
