eaf459c5fcd79dd4b3e5c84c85a81befb354942f
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-04-09 17:33:56 -0700

    Change back newline

--- zfc/main.cpp
+++ zfc/main.cpp
@@ -9,7 +9,7 @@
 
 int main(int argc, char** argv) {
     if (argc < 3) {
-        std::cerr << "Expected files (for input and output)" << std::endl;
+        std::cerr << "Expected files (for input and output)" << "\n";
         exit(-1);
     }
     compile(argv[1], argv[2]);
