2d03663f1e857d5d279756d455e00e1e1c4caabe
adamhutchings <adam.abahot@gmail.com>
2021-04-09 18:30:47 -0700

    Make file listing actually work

--- zfc/util/fs.cpp
+++ zfc/util/fs.cpp
@@ -2,27 +2,27 @@
 
 namespace fs {
 
-std::vector<std::string> get_compile_list(std::string dir, std::string prefix=std::string(".")) {
+std::vector<std::string> get_compile_list(std::string dir) {
     std::vector<std::string> ret;
     auto direc = opendir(dir.c_str());
     if (direc == NULL) {
-        throw "directory not found";
+        ZF_ERROR("could not find directory %s", dir.c_str());
     }
     struct dirent* entry;
     while ( (entry = readdir(direc)) != NULL ) {
         if (entry->d_type & DT_DIR) {
             // Make sure to skip . and ..
-            if (std::string(".") .compare(entry->d_name) |
-                std::string("..").compare(entry->d_name)) {
+            if (!(std::string(".") .compare(entry->d_name) &&
+                std::string("..").compare(entry->d_name))) {
                 ;
             } else {
-                auto sub = get_compile_list(prefix + entry->d_name + "/", std::string(entry->d_name) + "/");
+                auto sub = get_compile_list(dir + entry->d_name + "/");
                 for (auto& ent : sub) {
                     ret.push_back(ent);
                 }
             }
         } else {
-            ret.push_back(prefix + "/" + entry->d_name);
+            ret.push_back(dir + entry->d_name);
         }
     }
     closedir(direc);
