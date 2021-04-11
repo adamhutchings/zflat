34ebb3cc34fb3de137001f897da2286e1048e679
adamhutchings <adam.abahot@gmail.com>
2021-04-10 17:30:40 -0700

    Add more argument parsing

--- zfc/main.cpp
+++ zfc/main.cpp
@@ -8,9 +8,52 @@
 #include <compile.hpp>
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cerr << "Expected input file" << "\n";
+    ++argv, --argc;
+    bool one_file = false;
+    bool help = true;
+    std::string one_file_path = "";
+    std::vector<std::string> args;
+    while (argc-- > 0) {
+        args.push_back(std::string(*(argv++)));
+    }
+    for (auto str : args) {
+        if (str == "-I") {
+            if (one_file) {
+                std::cout << "zfc: warning: -I flag passed twice\n";
+            }
+            one_file = true;
+        } else if (str == "--help" || str == "-h") {
+            help = true;
+            break;
+        }
+        if (one_file) {
+            if (one_file_path == "") {
+                one_file_path = str;
+            } else {
+                std::cerr << "zfc: file path specified twice\n";
+                exit(-1);
+            }
+        } else {
+            std::cerr << "zfc: arguments unneeded in batch file mode\n";
+            exit(-1);
+        }
+    }
+    if (one_file && one_file_path == "") {
+        std::cerr << "zfc: expected file after -I flag\n";
         exit(-1);
     }
-    compile_unit(argv[1]);
+    if (help) {
+        std::cout << "zfc: Compiler for zflat\n"
+        "Usage:\n\t"
+        "zfc -I [file] (compile one file)\n\t"
+        "zfc (compile all files in src directory)\n"
+        "Flags:\n\t"
+        "--help: see this message\n";
+        exit(0);
+    }
+    if (one_file) {
+        compile_unit(one_file_path);
+    } else {
+        compile_default();
+    }
 }
