86770d3442455668abb586a1beadbd23a0bfc7d7
adamhutchings <adam.abahot@gmail.com>
2021-04-11 16:30:44 -0700

    Add switch gen

---
+++ zfc/back/gen/gen_switch.cpp
@@ -0,0 +1,23 @@
+#include <ast/ast.hpp>
+#include <back/gen/gen_main.hpp>
+
+void SwitchNode::write(std::ofstream& file) {
+
+    ControlFlowNode br;
+    br.statement = BREAK;
+    br.expression = nullptr;
+    
+    gen::write(file, "switch (");
+    this->expr->write(file);
+    gen::write(file, ") {");
+    gen::indent();
+    for (auto cs : this->cases) {
+        cs->write(file);
+        if (this->fswitch) {
+            gen::newline(file);
+            br.write(file);
+        }
+    }
+    gen::unindent();
+
+}
