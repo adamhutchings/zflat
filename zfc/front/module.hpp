094a0eb4c753ad26b79860a1fbe74d4af098b6ff
adamhutchings <adam.abahot@gmail.com>
2021-04-16 21:17:32 -0700

    Change ifstream to ofstream

--- zfc/front/module.hpp
+++ zfc/front/module.hpp
@@ -14,7 +14,7 @@ void writesym(std::ofstream& file, sym::Symbol* sym);
 void begin_read(std::ifstream& file);
 void end_read(std::ifstream& file);
 
-void begin_write(std::ifstream& file);
-void end_write(std::ifstream& file);
+void begin_write(std::ofstream& file);
+void end_write(std::ofstream& file);
 
 }
