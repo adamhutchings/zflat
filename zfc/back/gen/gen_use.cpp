cf5e1cd2527fc62c9e3a82d89f26aa64f0991859
adamhutchings <adam.abahot@gmail.com>
2021-04-17 18:58:48 -0700

    Add actual use gen

--- zfc/back/gen/gen_use.cpp
+++ zfc/back/gen/gen_use.cpp
@@ -3,6 +3,31 @@
 
 void UseNode::write(std::ofstream& file) {
 
-    // TODO - write all forward declarations
+    for (auto decl : this->symtab) {
+
+        auto* var = static_cast<sym::Variable*>(decl);
+        if (var != nullptr) {
+            gen::write(file, "extern ");
+            gen::write(file, var->type.to_output_str() + " " + var->name + ";");
+        }
+
+        auto* fun = static_cast<sym::Function*>(decl);
+        if (fun != nullptr) {
+            gen::write(file, fun->ret.to_output_str() + " " + fun->name + "(");
+            int ind = 0;
+            for (auto arg : fun->args) {
+                if (ind != 0) {
+                    gen::write(file, ", ");
+                }
+                gen::write(file, arg.type.to_output_str());
+            }
+            gen::write(file, ");");
+        }
+
+        gen::write(file, "\n");
+
+    }
+
+    gen::write(file, "\n");
 
 }
