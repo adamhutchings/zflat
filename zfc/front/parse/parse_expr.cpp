1c113a0ac6230350f68b2cefaa9987661a8d31f5
adamhutchings <adam.abahot@gmail.com>
2021-04-21 21:26:08 -0700

    (Probably) fix expr parsing

--- zfc/front/parse/parse_expr.cpp
+++ zfc/front/parse/parse_expr.cpp
@@ -5,94 +5,127 @@
 #include <front/lex.hpp>
 #include <util/error.hpp>
 
+namespace {
+
+/**
+ * Read a single expression, non-binary if non-parenthesized. It will return
+ * nullptr if no more exprs remain.
+ */
+InnerExprNode* single_node_read(std::ifstream& file) {
+    Token next = lex::lex(file);
+    Token next2;
+    ExprNode* expr;
+    InnerExprNode* iexpr;
+    switch (next.type) {
+        case OPAREN:
+            expr = new ExprNode();
+            expr->read(file);
+            // Expect closing paren.
+            next2 = lex::lex(file);
+            if (next2.type != CPAREN) {
+                ZF_TOK_ERR(next2, "')'");
+            }
+            iexpr = expr->inner;
+            delete expr;
+            return iexpr;
+        case IDENTIFIER:
+            // Either a variable or a function call.
+            next2 = lex::lex(file);
+            if (next2.type == OPAREN) {
+                // Function call.
+                lex::unlex(next2);
+                lex::unlex(next);
+                iexpr = new FuncCallNode();
+                iexpr->read(file);
+                return iexpr;
+            } else {
+                lex::unlex(next2);
+                iexpr = new VariableNode();
+                auto sym = sym::resolve_var(next.str);
+                if (sym = nullptr) {
+                    ZF_ERROR("line %d: could not resolve symbol %s", next.line, next.str);
+                }
+                dynamic_cast<VariableNode*>(iexpr)->sym = sym;
+                return iexpr;
+            }
+        case LITERAL:
+            iexpr = new LiteralNode();
+            dynamic_cast<LiteralNode*>(iexpr)->lit = next.str;
+            return iexpr;
+        default:
+            return nullptr;
+    }
+}
+
+}
+
 void ExprNode::read(std::ifstream& file) {
 
-    auto start = lex::lex(file);
-    
-    this->locked = false;
-    this->ref = nullptr;
+    /* Sketchy algorithm o-clock!
+     * from Binkiklou, I crown this Adam's Sketchy Expression-Parsing Algorithm.
+     * We read a vector of expression nodes and a vector of operators
+     * between them. The nodes shouldn't be tree nodes, so we organize them into
+     * a tree based on priority. However, if an expression is parenthesized, it
+     * will be parsed as one expression.
+     */
 
-    if (start.type == TreeComp::OPAREN) {
-        this->locked = true;
-        this->read(file);
-        // An extra closing ) should exist.
-        auto close = lex::lex(file);
-        if (close.type != TreeComp::CPAREN) {
-            ZF_TOK_ERR(close, ")");
-        }
-        auto next = lex::lex(file);
-        if (next.type == TreeComp::OPERATOR) {
-            // Assign "this" to this->left
-            // Copy the left node
-            ExprNode* left = this->left;
-            // Copy this to this->left
-            this->left = new ExprNode();
-            this->left->right = this->right;
-            this->left->left = left;
-            this->left->op = this->op;
-            this->left->ref = this->ref;
-            // Make a new right node
-            this->op = op::strToOp(next.str);
-            this->right = new ExprNode();
-            this->right->read(file);
-        } else {
-            lex::unlex(next);
-        }
-    } else if (start.type == TreeComp::LITERAL || start.type == TreeComp::IDENTIFIER) {
-        // We expect an operator or a function call.
-        auto next = lex::lex(file);
-        if (next.type == TreeComp::OPERATOR) {
-            this->line = next.line;
-            ExprNode* left_expr = new ExprNode();
-            if (start.type == TreeComp::LITERAL) {
-                left_expr->literal = start.str;
-                left_expr->ref = nullptr;
-            } else {
-                left_expr->ref = sym::resolve_var(start.str);
-                if (left_expr->ref == nullptr) {
-                    ZF_ERROR("could not resolve variable \"%s\" on line %d", start.raw_content(), start.line);
-                }
-                left_expr->literal = "";
+    std::vector<InnerExprNode*> nodes;
+    std::vector<op::Operator> ops;
+
+    // BEGIN - read operators and expressions
+    int nc = 0;
+    while (1) {
+        // Read an operator
+        if (nc != 0) {
+            auto opt = lex::lex(file);
+            if (opt.type != OPERATOR) {
+                ZF_TOK_ERR(opt, "operator");
             }
-            left_expr->left = left_expr->right = nullptr;
-            left_expr->op = op::Operator::INVALID;
-            this->left = left_expr;
-            this->op = op::strToOp(next.str);
-            this->literal = "";
-            this->ref = nullptr;
-            this->right = new ExprNode();
-            this->right->read(file);
-        } else if (next.type == TreeComp::OPAREN) {
-            // Put back this and last, parse function call
-            lex::unlex(next);
-            lex::unlex(start);
-            this->left = new FuncCallNode();
-            this->left->read(file);
-            this->literal = "";
-            this->op = op::Operator::INVALID;
-            this->right = nullptr;
-            this->locked = true;
-            this->line = this->left->line;
-        } else if (next.type == TreeComp::SEMICOLON || next.type == TreeComp::CPAREN || next.type == TreeComp::COMMA) {
-            this->literal = start.str;
-            this->left = this->right = nullptr;
-            this->op = op::Operator::INVALID;
-            // Put back the next token, which is not part of the expr.
-            lex::unlex(next);
-            this->line = start.line;
-        } else {
-            ZF_TOK_ERR(next, "operator, ';', or '('");
+            auto op = op::strToOp(opt.str);
+            if (op == op::Operator::INVALID) {
+                ZF_TOK_ERR(opt, "valid operator token");
+            }
+            ops.push_back(op);
         }
-    } else {
-        // Unary operators - not supported yet
-        ZF_TOK_ERR(start, "value");
+        // Read one atomic expression.
+        auto temp = single_node_read(file);
+        if (temp == nullptr) break;
+        nodes.push_back(temp);
+        ++nc;
     }
 
-    // Check to make sure left-hand side is an lvalue
-    if (this->op != op::Operator::INVALID && op::is_assign(this->op)) {
-        if (this->left->ref == nullptr) {
-            ZF_ERROR("line %d: invalid lvalue (expected identifier)", this->line);
+    // Now, we combine over and over.
+    while (nodes.size() > 1) {
+
+        // Traverse to find highest-precedence operator in the list.
+        int splice_loc = 0;
+        int size = ops.size();
+        for (int i = 0; i < size; ++i) {
+            if (op::binding_order(ops[splice_loc], ops[i]) == 1) {
+                splice_loc = i;
+            }
         }
+
+        // Now we combine the two nodes adjacent to splice_loc.
+        InnerExprNode* treefrag = new BinaryExprNode();
+        BinaryExprNode* tree = dynamic_cast<BinaryExprNode*>(treefrag);
+        tree->left = nodes[splice_loc];
+        tree->right = nodes[splice_loc + 1];
+        tree->op = ops[splice_loc];
+
+        // Remove the two old nodes from the list.
+        // This is meant to be doubled.
+        nodes.erase(nodes.begin() + splice_loc);
+        nodes.erase(nodes.begin() + splice_loc);
+
+        // Remove old op.
+        ops.erase(ops.begin() + splice_loc);
+
+        // Add new element to nodes list.
+        nodes.insert(nodes.begin() + splice_loc, treefrag);
+
     }
+
+    this->inner = nodes[0];
 
 }
