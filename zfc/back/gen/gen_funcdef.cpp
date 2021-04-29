49e731fad59adda00b64da87e46fed6b78d6d6ff
adamhutchings <adam.abahot@gmail.com>
2021-04-29 13:16:17 -0700

    Only output names if not unnamed

--- zfc/back/gen/gen_funcdef.cpp
+++ zfc/back/gen/gen_funcdef.cpp
@@ -9,10 +9,15 @@ void FunctionNode::write(std::ofstream& file) {
     gen::write(file, (this->symbol->extc || this->symbol->name == "main") ? this->symbol->name : this->symbol->get_overloaded_name());
 
     gen::write(file, "(");
-    for (auto arg : this->symbol->args) {
-        if (arg.name != this->symbol->args[0].name)
+    int size = this->symbol->args.size();
+    for (int i = 0; i < size; ++i) {
+        auto arg = this->symbol->args[i];
+        if (i != 0)
             gen::write(file, ",");
-        gen::write(file, arg.type.to_output_str() + " " + arg.name);
+        gen::write(file, arg.type.to_output_str() + " ");
+        if (arg.name != "$unnamed") {
+            gen::write(file, arg.name);
+        }
     }
     gen::write(file, ") ");
 
