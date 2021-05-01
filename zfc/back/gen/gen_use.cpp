650ca2eb2bc84d23e661df993d5e0d459a56069b
adamhutchings <adam.abahot@gmail.com>
2021-05-01 07:10:14 -0700

    Use switch/case to be warned about values

--- zfc/back/gen/gen_use.cpp
+++ zfc/back/gen/gen_use.cpp
@@ -5,14 +5,14 @@ void UseNode::write(std::ofstream& file) {
     for (auto decl : this->symtab) {
 
         auto* var = static_cast<sym::Variable*>(decl);
-        if (decl->s_type == sym::SymType::VAR) {
+        auto* fun = static_cast<sym::Function*>(decl);
+
+        switch (decl->s_type) {
+        case sym::SymType::VAR:
             gen::write(file, "extern ");
             gen::write(file, var->type.to_output_str() + " " + var->name + ";");
             return;
-        }
-
-        auto* fun = static_cast<sym::Function*>(decl);
-        if (decl->s_type == sym::SymType::FN) {
+        case sym::SymType::FN:
             gen::write(file, fun->ret.to_output_str() + " " + fun->name + "(");
             int ind = 0;
             for (auto arg : fun->args) {
