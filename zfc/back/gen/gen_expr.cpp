74e64961bb503dcd0a5879aa6823b55e0cd7e84b
adamhutchings <adam.abahot@gmail.com>
2021-05-07 23:01:42 -0700

    Generate literal enum values

--- zfc/back/gen/gen_expr.cpp
+++ zfc/back/gen/gen_expr.cpp
@@ -22,7 +22,19 @@ void LiteralNode::write(std::ofstream& file) {
 void VariableNode::write(std::ofstream& file) {
     if (this->ref) gen::write(file, "& ");
     if (this->sym->type->ref) gen::write (file, "* ");
-    gen::write(file, this->sym->name);
+    if (this->sym->type->type_flavor == TT_ENUM) {
+        // Generate literal values as literals rather than constants
+        auto esym = static_cast<Enum*>(this->sym->type);
+        for (auto v : esym->values) {
+            auto evsym = reinterpret_cast<sym::EnumVal*>(v);
+            if (evsym->name == this->sym->name) {
+                gen::write(file, std::to_string(evsym->val));
+                break;
+            }
+        }
+    } else {
+        gen::write(file, this->sym->name);
+    }
 }
 
 void ExprNode::write(std::ofstream& file) {
