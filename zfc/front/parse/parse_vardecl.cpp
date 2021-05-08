db94382e56c08be1263f6650af3d872359f55b62
adamhutchings <adam.abahot@gmail.com>
2021-05-07 22:49:42 -0700

    Fix 'left' / 'right' swap in test

--- zfc/front/parse/parse_vardecl.cpp
+++ zfc/front/parse/parse_vardecl.cpp
@@ -18,7 +18,7 @@ void VarDeclNode::read(std::ifstream& file) {
         }
         this->expr = new ExprNode();
         this->expr->read(file);
-        auto left = get_type(this->expr), right = this->var->type;
+        auto left = this->var->type, right = get_type(this->expr);
         if (!are_types_compatible(left, right)) {
             ZF_ERROR("line %d: assigning value of type %s to var of type %s"
             , this->line, left->to_human_str().c_str(), right->to_human_str().c_str());
