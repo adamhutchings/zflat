5d4775a817b920f306c6e36fed8c8418fe938251
adamhutchings <adam.abahot@gmail.com>
2021-05-10 16:06:07 -0700

    Refactor lexer and add comments

--- zfc/front/lex.cpp
+++ zfc/front/lex.cpp
@@ -15,6 +15,9 @@ const char* Token::raw_content() {
 
 namespace {
 
+// These are misnomers - they refer to the last, present, and next characters.
+char lasttok = 0, curtok = 0, nexttok = 0;
+
 // Whether in the INSIDE of a comment or a quote.
 bool in_comment = false;
 
@@ -78,22 +81,32 @@ bool is_always_tok(char c) {
     }
 }
 
+// The maximum langth of a token
 const int TOK_MAX = 256;
 
 // As characters are read from the file, they get placed into here - when the
-// end of a token is reachedd, all characters are popped into a string and
+// end of a token is reached, all characters are popped into a string and
 // returned.
 char cbuf[TOK_MAX + 1];
+
+// This is the character PAST the last character in the token.
 int cend = 0;
+
+// eofhit - whether the end-of-file is currently detected.
+// lastwascolon - if the last token to be lexed was a colon.
+// whether we are currently in a quote.
+bool eofhit = false, lastwascolon = false, in_quote = false;
 
 void add_char(char c) {
     cbuf[cend++] = c;
 }
 
-// These are misnomers - they refer to the last, present, and next characters.
-char lasttok = 0, curtok = 0, nexttok = 0;
-
-bool eofhit = false, lastwascolon = false, in_quote = false;
+// Set the end to 0 and clear the buffer
+void reset_buf_state() {
+    cend = 0;
+    if (cbuf[0] == '\"') in_quote = false;
+    memset(cbuf, 0, sizeof(char) * TOK_MAX + 1);
+}
 
 int line = 1, linepos = 1;
 
@@ -129,6 +142,30 @@ bool ends_token(char cur, char next) {
     if (is_op_token(cur))           return (next != '=');
     if (is_always_tok(cur))         return true;
     if (is_always_tok(next))        return true;
+    return false;
+}
+
+TreeComp get_keyword_type(std::string name) {
+    if (name == "loop")          return TreeComp::LOOP;
+    if (name == "if")            return TreeComp::IF;
+    if (name == "else")          return TreeComp::ELSE;
+    if (name == "switch")        return TreeComp::SWITCH;
+    if (name == "case")          return TreeComp::CASE;
+    if (name == "fswitch")       return TreeComp::FSWITCH;
+    if (name == "extc")          return TreeComp::EXTC;
+    if (name == "__c_va_args")   return TreeComp::VA_ARGS;
+    if (name == "use")           return TreeComp::USE;
+    if (name == "ref")           return TreeComp::REF;
+    if (name == "enum")          return TreeComp::ENUM;
+    if (name == "bitfield")      return TreeComp::BITFIELD;
+    return TreeComp::EGENERALERROR;
+}
+
+bool is_builtin_type(std::string name) {
+    for (int typei = 0; typei < static_cast<int>(BuiltinType::MAX_INVALID); ++typei) {
+        auto type = static_cast<BuiltinType>(typei);
+        if (typeToZStr(type) == name) return true;
+    }
     return false;
 }
 
@@ -174,24 +211,9 @@ TreeComp get_type(std::string name) {
     }
     if (name == "break" || name == "continue" || name == "return")
         return TreeComp::FLOW;
-    if (
-        name == "int" || name == "double" || name == "float" || name == "char"
-    ||  name == "short" || name == "long" || name == "bool" || name == "void"
-    || name == "ushort" || name == "uint" || name == "ulong" || name == "uchar"
-    ) return TreeComp::TYPENAME;
-    // A bunch of keywords.
-    if (name == "loop")          return TreeComp::LOOP;
-    if (name == "if")            return TreeComp::IF;
-    if (name == "else")          return TreeComp::ELSE;
-    if (name == "switch")        return TreeComp::SWITCH;
-    if (name == "case")          return TreeComp::CASE;
-    if (name == "fswitch")       return TreeComp::FSWITCH;
-    if (name == "extc")          return TreeComp::EXTC;
-    if (name == "__c_va_args")   return TreeComp::VA_ARGS;
-    if (name == "use")           return TreeComp::USE;
-    if (name == "ref")           return TreeComp::REF;
-    if (name == "enum")          return TreeComp::ENUM;
-    if (name == "bitfield")      return TreeComp::BITFIELD;
+    if (is_builtin_type(name)) return TreeComp::TYPENAME;
+    auto kwt = get_keyword_type(name);
+    if (kwt != EGENERALERROR) return kwt;
     if (is_alpha(name[0])) {
         return (lastwascolon) ? TreeComp::TYPENAME : TreeComp::IDENTIFIER;
     }
@@ -201,61 +223,95 @@ TreeComp get_type(std::string name) {
     ZF_ERROR("invalid material for token: on line %d, found %s", line, name.c_str());
 }
 
-// A put-back tokens lands in here.
-std::vector<Token> put_back_tokens;
-
-Token process_character(std::ifstream& file) {
-    if (eofhit) {
-        if (in_comment) ZF_ERROR("Open comment");
-        return Token(TreeComp::TEOF, "[end of file]");
+// Edit the backslash count for quote parsing according to the current state.
+inline void manage_backslashes() {
+    if (curtok == '\\') {
+        ++bsct;
+    } else {
+        bsct = 0;
     }
-    lasttok = curtok;
-    curtok = file.get();
-    nexttok = file.peek();
+}
+
+// Edit the line and linepos counts according to the current state.
+inline void manage_line_and_linepos() {
     if (curtok == '\n') {
         ++line;
         linepos = 1;
     } else {
         ++linepos;
     }
+}
+
+// Move the character set one position ahead in the file.
+inline void advance_characters(std::ifstream& file) {
+    lasttok = curtok;
+    curtok = file.get();
+    nexttok = file.peek();
+}
+
+inline void update_eofflag(std::ifstream& file) {
+    if (file.peek() == EOF)
+        eofhit = true;
+}
+
+inline void reset_token_flags() {
+    in_comment = false;
+    in_quote = false;
+}
+
+inline void update_comment_flags() { 
+    if (curtok == '~') {
+        in_comment = true;
+    }
+}
+
+// Whether a character should be added to the buffer or ignored.
+inline bool yes_add_char(char c) {
+    return (!is_whitespace(c) && !in_comment) || in_quote;
+}
+
+// A put-back token lands in here.
+// The next call to lex::lex will scan this buffer first.
+std::vector<Token> put_back_tokens;
+
+Token process_character(std::ifstream& file) {
+    if (eofhit) {
+        if (in_comment) ZF_ERROR("Open comment");
+        if (in_quote) ZF_ERROR("Open quote");
+        // Return this every time and don't bother going through the motions.
+        return Token(TreeComp::TEOF, "[end of file]");
+    }
+    advance_characters(file);
+    manage_line_and_linepos();
     if (curtok == '\"' && !in_quote) {
         in_quote = true;
     }
     if (cend >= TOK_MAX)
         ZF_ERROR("token on line %u exceeded limit", line);
-    if (file.peek() == EOF) eofhit = true;
-    if (ends_token(curtok, nexttok)) {
+    update_eofflag(file);
+    if (begins_token(lasttok, curtok)) {
+        // Clearing the buffer state - this should theoretically be
+        // unnecessary, but do it anyway in case an edge case comes up.
+        reset_buf_state();
+    }
+    if (yes_add_char(curtok)) {
         add_char(curtok);
+    }
+    if (ends_token(curtok, nexttok)) {
+        // Clear the buffer, but first create a token and return it.
         Token tok(get_type(cbuf), cbuf);
         lastwascolon = curtok == ':';
-        cend = 0;
-        if (cbuf[0] == '\"') in_quote = false;
-        memset(cbuf, 0, sizeof(char) * TOK_MAX + 1);
-        if (curtok == '~') {
-            in_comment = false;
-        }
-        in_quote = false;
+        reset_token_flags();
         return tok;
-    } else {
-        if (begins_token(lasttok, curtok)) {
-            cend = 0;
-        }
-        if (curtok == '~') {
-            in_comment = !in_comment;
-        }
-        if ((!is_whitespace(curtok) && !in_comment) || in_quote) {
-            add_char(curtok);
-        }
     }
-    if (curtok == '\\') {
-        ++bsct;
-    } else {
-        bsct = 0;
-    }
+    update_comment_flags();
+    manage_backslashes();
+    // Otherwise, return an invalid token.
     return Token(TreeComp::EGENERALERROR, "");
 }
 
 Token inner_get_token(std::ifstream& file) {
+    // Go over characters one at a time until the routine returns a valid token.
     Token ret;
     while ( (ret = process_character(file)).type == TreeComp::EGENERALERROR );
     ret.line = line;
@@ -267,10 +323,12 @@ Token inner_get_token(std::ifstream& file) {
 
 Token lex::lex(std::ifstream& file) {
     Token tok;
+    // Scan put-back buffer first.
     if (put_back_tokens.size() > 0) {
         tok = put_back_tokens[put_back_tokens.size() - 1];
         put_back_tokens.pop_back();
     }
+    // Don't bother with comments. This was a hacky fix from a while ago.
     else
         while ( (tok = inner_get_token(file)).type == TreeComp::COMMENT );
     return tok;
@@ -284,6 +342,7 @@ void lex::reset_lexer() {
     line = 1;
     linepos = 1;
     put_back_tokens.clear();
-    memset(cbuf, 0, sizeof(char) * TOK_MAX + 1);
+    reset_buf_state();
+    reset_token_flags();
     eofhit = false;
 }
