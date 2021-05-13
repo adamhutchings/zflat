0751892c399fe252ea64a40988c67b49f5dcae01
adamhutchings <adam.abahot@gmail.com>
2021-05-12 17:14:53 -0700

    Add debug levels

---
+++ zfc/common/debug.hpp
@@ -0,0 +1,13 @@
+/**
+ * debug - utilities for the debug values and constants
+ */
+
+#pragma once
+
+namespace debug {
+
+extern unsigned int level;
+
+extern const unsigned int level_default, level_max;
+
+}
