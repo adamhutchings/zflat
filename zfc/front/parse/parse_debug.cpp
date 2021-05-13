911659deb95ac952af6030801d9fb67ff3db4fce
adamhutchings <adam.abahot@gmail.com>
2021-05-12 17:21:33 -0700

    Fix unused variable warnings

--- zfc/front/parse/parse_debug.cpp
+++ zfc/front/parse/parse_debug.cpp
@@ -12,16 +12,16 @@ void DebugNode::read(std::ifstream& file) {
         case TreeComp::OPAREN:
             // Parse the value after
             lit = expect(file, LITERAL);
-            int val;
             try {
                 int val = std::stoi(lit.str);
+                // Alright, make sure it's in range.
+                if (val > debug::level_max) {
+                    ZF_TOK_ERR(lit, "a value in the range for debug ranges");
+                }
+                this->debug_val = val;
             } catch (std::invalid_argument a) {
                 // Not a valid literal!
                 ZF_TOK_ERR(lit, "a valid integer literal");
-            }
-            // Alright, make sure it's in range.
-            if (val > debug::level_max) {
-                ZF_TOK_ERR(lit, "a value in the range for debug ranges");
             }
             expect(file, CPAREN);
             // Fall-through to the obrace statement. Assigning to nx so the
