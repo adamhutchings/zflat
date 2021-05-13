9801387e9d0257d2899321fb52c3e8d0ad9ed6e5
adamhutchings <adam.abahot@gmail.com>
2021-05-12 17:17:49 -0700

    Add debug generation

---
+++ zfc/back/gen/gen_debug.cpp
@@ -0,0 +1,14 @@
+#include <common/debug.hpp>
+#include <gen/gen_main.hpp>
+
+void DebugNode::write(std::ofstream& file) {
+    
+    gen::write(file, "#if ZF_DEBUG_LEVEL >= ");
+    gen::write(file, std::to_string(this->debug_val));
+    gen::write(file, "\n");
+
+    this->block->write(file);
+
+    gen::write(file, "\n#endif /* DEBUG */\n");
+
+}
