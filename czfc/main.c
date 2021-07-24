5fa8cb9d4c846b089291abc294da4d8c9d52e067
adamhutchings <adam.abahot@gmail.com>
2021-07-23 17:30:07 -0700

    Call arg parsing functions from main

--- czfc/main.c
+++ czfc/main.c
@@ -2,6 +2,13 @@
  * czfc main file.
  */
 
+#include <args.h>
+
 int main(int argc, char ** argv) {
-    
+
+    struct zf_args args;
+
+    zf_args_init_program_name(&argv, &argc);
+    zf_args_parse(&args, argc, argv);
+
 }
