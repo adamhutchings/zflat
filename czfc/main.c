37334994cf68fdc7abdbe34319e8850cc31d26c3
adamhutchings <adam.abahot@gmail.com>
2021-07-28 12:28:48 -0700

    Indirectly call the core compiler

--- czfc/main.c
+++ czfc/main.c
@@ -3,6 +3,7 @@
  */
 
 #include <args.h>
+#include <compile.h>
 
 int main(int argc, char ** argv) {
 
@@ -11,5 +12,6 @@ int main(int argc, char ** argv) {
     zf_args_init_program_name(&argv, &argc);
     zf_args_parse(&args, argc, argv);
     zf_output_peripherals(&args, argv);
+    zf_execute_args(&args);
 
 }
