60ec1a003a03b73e1bf9c547b0875b457afad2b3
adamhutchings <adam.abahot@gmail.com>
2021-07-28 09:58:01 -0700

    Add character tests

--- czfc/front/lex.c
+++ czfc/front/lex.c
@@ -1,5 +1,6 @@
 #include "lex.h"
 
+#include <ctype.h>  /* isspace, etc */
 #include <string.h> /* memset */
 
 #include <output.h> /* error macros */
@@ -37,4 +38,43 @@ unsigned zf_lexer_init(struct zf_lexer * lexer, const char * filename) {
 
 void zf_lexer_destroy(struct zf_lexer * lexer) {
     fclose(lexer->fp);
+}
+
+/**
+ * Internal use flags - these represent different types of characters.
+ */
+static enum {
+    ZFL_WHITESPACE          = 0x01,
+    ZFL_ALPHA               = 0x02,
+    ZFL_DIGIT               = 0x04,
+    ZFL_CSYMBOL             = 0x08,
+};
+
+/**
+ * A function that tests if a character matches some criterion.
+ */
+typedef int                 (*zf_lex_test)(int c);
+
+/**
+ * Get the function for a given criterion. This only gets ONE criterion at a 
+ * time - do not pass ZFL_WHITESPACE | ZFL_ALPHA.
+ */
+static zf_lex_test zf_lex_get_test(int flag) {
+
+    switch (flag) {
+        case ZFL_WHITESPACE:
+            return isspace;
+        case ZFL_ALPHA:
+            return isalpha;
+        case ZFL_DIGIT:
+            return isdigit;
+        case ZFL_CSYMBOL:
+        /* This may nnot be 100% accurate - (see backslashes). We'll be able to
+         * catch the error (hey! \ isn't an operator!) but a more thorough
+         * solution may be in order later. */
+            return ispunct;
+        default:
+            return NULL;
+    }
+
 }
