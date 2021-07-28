42d94b73ca4b0ebd336007552fce1592b5d72b20
adamhutchings <adam.abahot@gmail.com>
2021-07-27 20:54:21 -0700

    Add lexer_init function

---
+++ czfc/front/lex.c
@@ -0,0 +1,22 @@
+#include "lex.h"
+
+#include <string.h> /* memset */
+
+#include <output.h> /* error macros */
+
+unsigned zf_lexer_init(struct zf_lexer * lexer, const char * filename) {
+
+    memset(lexer, 0, sizeof *lexer);
+
+    /* Linepos start at 1, column start at 0 */
+    lexer->linepos = 1;
+
+    lexer->fp = fopen(filename, "r");
+    if (lexer->fp == NULL) {
+        ZF_PRINT_WITH_COLOR(ZF_CODE_RED, "Cannot open file \"%s\"", filename);
+        return 1;
+    }
+
+    return 0;
+
+}
