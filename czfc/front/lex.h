85d1c1ca7d20d112a783a5519baf74b78ecfb2d9
adamhutchings <adam.abahot@gmail.com>
2021-07-27 20:47:14 -0700

    Add token and lexer structs

--- czfc/front/lex.h
+++ czfc/front/lex.h
@@ -3,3 +3,32 @@
  */
 
 #pragma once
+
+/* I could see really, really long quotes being about this long. */
+#define ZF_TOKEN_LEX_MAX 512
+
+struct zf_token {
+
+    /* The string content of the token. */
+    char                      data[ZF_TOKEN_LEX_MAX];
+
+    /* What line number the token was on (for printing diagnostics and such) */
+    unsigned                  lineno;
+
+    /* What possibly-empty string the token was on */
+    unsigned                  linepos;
+
+    /* TODO - token types */
+
+};
+
+struct zf_lexer {
+
+    /* The file we're lexing. */
+    FILE                    * fp;
+
+    /* Current line number and position. */
+    unsigned                  lineno
+                            , linepos;
+
+};
