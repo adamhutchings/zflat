c4c6e59d8d00a3409070c63a9bfbe674923509cc
adamhutchings <adam.abahot@gmail.com>
2021-07-28 15:34:16 -0700

    Add hashtable impl

---
+++ czfc/util/hashtable.c
@@ -0,0 +1,29 @@
+#include "hashtable.h"
+
+/**
+ * Hashing algorithm - this is chosen more or less at random.
+ */
+static uint32_t zf_hash(const char * str) {
+
+    uint32_t hash = 19329;
+
+    while (*str) {
+        hash = ((hash << 7) + hash) + *str;
+        ++str;
+    }
+
+    return hash;
+
+}
+
+void zf_hashtable_set(
+    struct zf_hashtable * ht, const char * key, void * value
+) {
+    uint32_t hash = zf_hash(key) % ZF_HASH_BUCKETS;
+    ht->buckets[hash] = value;
+}
+
+void * zf_hashtable_get(struct zf_hashtable * ht, const char * key) {
+   uint32_t hash = zf_hash(key) % ZF_HASH_BUCKETS;
+   return ht->buckets[hash];
+}
