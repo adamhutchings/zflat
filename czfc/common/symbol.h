9f3356c822a585a869e1d047a0a5089a15b3a9fa
adamhutchings <adam.abahot@gmail.com>
2021-08-24 15:02:29 -0700

    Add some symbol headers

---
+++ czfc/common/symbol.h
@@ -0,0 +1,45 @@
+/**
+ * A reference to a variable, a function, or (later) a type.
+ */
+
+#pragma once
+
+#include <util/linkedlist.h>
+
+enum zfs_kind {
+    ZFS_VARIABLE,
+    ZFS_FUNCTION,
+    ZFS_MAX,
+};
+
+struct zfs_var {
+
+    /* Just the lastmost element. */
+    char                    * localname;
+
+    /* TODO - proper type reference */
+    char                    * type;
+
+};
+
+struct zfs_func {
+    char                    * name;
+    char                    * rettype;
+    struct zf_linked_list   * paramtypes;
+};
+
+struct zfs_sym {
+
+    union {
+        struct zfs_var      * var;
+        struct zfs_func     * func;
+    } as;
+
+    zfs_kind                  type;
+
+    /* What file this is from, for error messages, etc. */
+    char                    * srcfile;
+    /* Same deal. */
+    int                       srcline, srclinepos;
+
+};
---
+++ czfc/common/symtab.h
@@ -0,0 +1,41 @@
+/**
+ * A complete symbol table for a program.
+ */
+
+#pragma once
+
+#include <symbol.h>
+#include <util/hashtable.h>
+#include <util/linkedlist.h>
+
+struct zf_symtab;
+
+/**
+ * Enter a new scope.
+ */
+void zfs_enter_scope(struct zf_symtab * symtab);
+
+/**
+ * Exit the current scope.
+ */
+void zfs_exit_scope(struct zf_symtab * symtab);
+
+/**
+ * Add a new definition. Returns 1 and prints an error if something happened
+ * (e.g. symbol already exists, etc.)
+ */
+int zfs_add_def(
+    struct zf_symtab * symtab,
+    const char * name,
+    struct zf_symbol * sym
+);
+
+/**
+ * Get the definition of a symbol. Returns 1 and prints an error if the symbol
+ * was not found.
+ */
+int zfs_get_def(
+    struct zf_symtab * symtab,
+    const char * name,
+    struct zf_symbol * sym
+);
